30630ef8827fe1727bb05c39d27b40ccd57357bc
Alaparthi Shasank sai <shasanksai@gmail.com>
2023-09-14 23:55:58 +0530

    twitter

---
+++ server/routes/users.js
@@ -0,0 +1,28 @@
+import express from "express";
+import {
+  getUser,
+  update,
+  deleteUser,
+  follow,
+  unFollow,
+} from "../controllers/user.js";
+import { verifyToken } from "../verifyToken.js";
+
+const router = express.Router();
+
+// Update User
+router.put("/:id",update);
+
+// Get User
+router.get("/find/:id", getUser);
+
+// Delete User
+router.delete("/:id", deleteUser);
+
+// Follow
+router.put("/follow/:id",  follow);
+
+// Unfollow
+router.put("/unfollow/:id",unFollow);
+
+export default router;
